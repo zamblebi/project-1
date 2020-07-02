76707772f5e2e9abf10cbb4a077afaf9dfb301bf
Samuel Bouye <bouyesamuel@gmail.com>
2020-07-02 16:44:54 +0000

    servces serction and about-us section

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -36,14 +36,23 @@
             </div>
             
             {{-- Section services --}}
-            <section class="services">
-                <div>
-                    <h1>Comment ca marche ?</h1>
+            <section >
+                <div class="services">
+                    <h1 class="title title-services">Comment ca marche ?</h1>
                     <div class="list-service">
                         <ul>
-                            <li><img src="" alt=""></li>
-                            <li><img src="" alt=""></li>
-                            <li><img src="" alt=""></li>
+                            <li>
+                                <img class="icon-item" src="/icons/phone.svg" alt="phone">
+                                <p>Réservez en ligne votre créneau</p>
+                            </li>
+                            <li>
+                                <img class="icon-item" src="/icons/iron.svg" alt="iron">
+                                <p>Confiez-nous votre linge</p>
+                            </li>
+                            <li>
+                                <img class="icon-item" src="/icons/suit.svg" alt="suit">
+                                <p>Récupérez votre linge propre </p>
+                            </li>
                         </ul>
                     </div>
                 </div>
@@ -52,11 +61,40 @@
             {{-- Section About us  --}}
             <section class="about-us">
                 <div class="about-us-list">
-                    <p>Les services d'un pressing de haute qualite a votre porte</p>
+                    <h1 class="title title-about-us">Pressing</h1>
+                <p class="small-details-title">Les services d'un pressing de haute qualite a votre porte</p>
                     <ul>
-                        <li><img src="" alt=""></li>
-                        <li><img src="" alt=""></li>
-                        <li><img src="" alt=""></li>
+                        <li class="about-item">
+                            <img src="/icons/thunder.svg" alt="">
+                            <div class="details">
+                                <p class="small-title">En un clin d'oeil</p>
+                                <p class="small-details">
+                                    Gagnez du temps !
+                                    Nous récupérons, lavons et livrons vos vêtements en 48h, vous profitez.
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="/icons/quality.svg" alt="">
+                            <div class="details">
+                                <p class="small-title">40 ans de qualitél</p>
+                                <p class="small-details">
+                                    Pressing haute qualité depuis 40 ans.
+                                    Maintenant profitez de nos services, sans vous déplacer !
+                                </p>
+                            </div>
+                        </li>
+                        <li>
+                            <img src="/icons/delivery.svg" alt="">
+                            <div class="details">
+                                <p class="small-title">Jusqu'à votre porte</p>
+                                <p class="small-details">
+                                    Le pressing c'est nous,
+                                    la livraison c'est Stuart.
+                                    Deux experts à votre service !
+                                </p>
+                            </div>
+                            </li>
                     </ul>
                 </div>
             </section>
