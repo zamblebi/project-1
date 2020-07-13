46e38b4a61c1a27cb1dcfaca4d0648aeea29e35e
Samuel Bouye <bouyesamuel@gmail.com>
2020-07-13 01:00:07 +0000

    list of clothing displaying dinamicaly

--- resources/js/components/ClothingComponent.vue
+++ resources/js/components/ClothingComponent.vue
@@ -1,17 +1,69 @@
 <template>
     <div>
-        <h1>{{message}}</h1>
+
+
+            <h2>Choisir une quantiter</h2>
+    <div class="order-clothing-list">
+        <ul class="list-clothing">
+                <li v-for="clothing in clothings" :key="clothing.id">
+                <img v-bind:src="'/icons/'+clothing.icon+'.svg'" alt="">
+                    <h3>
+                        {{clothing.name}}
+                    </h3>
+                    <div>
+                        <em>{{ clothing.prix }}f</em>
+                        <br>
+                        <strong>
+                            <!-- <em>Quantiter : {{!empty($getSession->items[$clothing->id]['qty']) ? $getSession->items[$clothing->id]['qty'] : '0'}}</em> -->
+                        </strong>
+                    </div>
+                    <br>
+
+                     <!-- Le bouton pour ajouter un vetement a la cart -->
+                        <a href="#" class="btn">Ajouter</a>
+                </li>
+
+        </ul>
+
+        <div>
+            <h3>Estimation du prix (hors livraison)</h3>
+            <hr>
+            <strong>
+             <!-- Voir la quantiter total -->
+                <p>Quantiter Total : </p>
+                    <!-- <p>{{Session::has('cart') ? Session::get('cart')->totalPrice : '0' }}f</p> -->
+            </strong>
+
+            <hr>
+        <a href="#" class="btn">Passer la commande</a>
+        </div>
+    </div>
+
+
+
+
+
+
+        <ul>
+            <!-- <li >{{clothing.name}}</li> -->
+        </ul>
     </div>
 </template>
 
 
 <script>
+    import axios from 'axios'
     export default{
 
         data: function(){
             return{
-                message: 'Salut depuis vue'
+                clothings: ''
             }
+        },
+        mounted(){
+            axios.get('/clothing-lists')
+            .then(response => (this.clothings = response.data))
+            .catch(error => console.log("Il y'a eu une erreur a la reccuperation " + error))
         }
     }
 </script>
--- resources/views/orders/index.blade.php
+++ resources/views/orders/index.blade.php
@@ -12,22 +12,9 @@
 <div class="wrapper " id="app">
 
     <clothing-component></clothing-component>
-    {{-- <h2>Choisir une quantiter</h2>
-<h3>{{session('success')}}</h3>
-    <div class="order-clothing-list">
-        <ul class="list-clothing">
-            @foreach ($clothings as $clothing)
-                <li>
-                <img src="/icons/{{$clothing->icon}}.svg" alt="">
-                    <h3>
-                        {{ $clothing->name }}
-                    </h3>
-                    <div>
-                        <em>{{ $clothing->prix }}f</em>
-                        <br>
-                        <strong>
-                            {{-- {{dd(get)}} --}}
-           
+  
+</div>
+
 <div id="footer">
     @include('includes.footer')
 </div>
