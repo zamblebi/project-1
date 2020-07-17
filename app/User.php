5a573d1df6349b32a79709ad07593869c92f9476
Samuel Bouye <bouyesamuel@gmail.com>
2020-07-17 10:16:10 +0000

    implement reset password in user model

--- app/User.php
+++ app/User.php
@@ -5,6 +5,7 @@ namespace App;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\CanResetPassword;
 
 class User extends Authenticatable
 {
@@ -37,7 +38,7 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    
+
     public function orders(){
         return $this->hasMany('App\Order');
     }
