2f43b0ffdb459fe171a59573ad38d1cf9326d793
Samuel Bouye <bouyesamuel@gmail.com>
2020-07-30 11:06:36 +0000

    fix ortographe deconnection

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -87,7 +87,7 @@
                                                 <a class="dropdown-item" href="{{ route('logout') }}"
                                                 onclick="event.preventDefault();
                                                             document.getElementById('logout-form').submit();">
-                                               {{ __('Deconnection') }}
+                                               {{ __('Déconnexion') }}
                                             </a>
                                         </li>
 
@@ -147,7 +147,7 @@
                                        <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                         document.getElementById('logout-form').submit();">
-                                           {{ __('Deconnection') }}
+                                           {{ __('Déconnexion') }}
                                        </a>
                                    </li>
 
